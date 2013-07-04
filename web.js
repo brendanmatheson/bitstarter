aa90614e18fdb07a069cbe05538e9613a4852bf1
Brendan Matheson <bmatheson@gmail.com>
2013-07-04 18:15:27 +0000

    Updated web.js to include index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var rFile =  fs.readFileSync("index.html");
+
+var output = rFile.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;

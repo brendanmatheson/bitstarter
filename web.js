5c8bbb5225b04d19c5b37dc973c8f84ffd5adbe6
Brendan Matheson <bmatheson@gmail.com>
2013-07-23 14:21:36 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html"
+
 var app = express.createServer(express.logger());
+
 var rFile =  fs.readFileSync("index.html");
 
 var output = rFile.toString();
 
 app.get('/', function(request, response) {
-  response.send(output);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
